eafb549ef55893befa928dda4d5d1f9c81e67ce2
BillChou <bill.chou@nogle.com>
2022-04-20 15:26:44 +0800

    調整code

--- src/main/java/com/bill/Storage.java
+++ src/main/java/com/bill/Storage.java
@@ -10,7 +10,13 @@ public class Storage {
     private static final int MAX_COUNT = 10; //倉庫最大數量
     private List<Production> productions = new ArrayList<>();  //公共資源
 
-    private ReentrantLock reentrantLock = new ReentrantLock();
+    /*
+     *  可以利用公平鎖觀察執行的效果會如何
+     *  private ReentrantLock reentrantLock = new ReentrantLock(true);
+     */
+    private ReentrantLock reentrantLock = new ReentrantLock();  //利用鎖控制produce、consume
+//    private ReentrantLock reentrantLock = new ReentrantLock(true);  //利用鎖控制produce、consume
+
     private Condition condition = reentrantLock.newCondition();
     private int index = 0;
 
@@ -21,12 +27,14 @@ public class Storage {
                 System.out.println("prodece await");
                 condition.await();
             }
-            Thread.sleep((long) Math.random() *10000 + 1000);   //隨機耗時
+            Thread.sleep(1000);   //耗時1秒
             Production production = new Production(index++);
+
             System.out.println("producer produce: "+ production.toString());
             productions.add(production);
-            condition.signal();
+            //Do Something...
 
+            condition.signal();
         } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
@@ -41,9 +49,12 @@ public class Storage {
                 System.out.println("consume await");
                 condition.await();  //貨物不足時停止消費
             }
-            Thread.sleep((long) Math.random() * 10000 + 1000);  //隨機耗時
+            Thread.sleep(1000);  //耗時1秒
             Production production = productions.remove(0);
+
             System.out.println("consumer consume: " + production.toString());
+            //Do Something...
+
             condition.signal(); //發個信號通知生產者
         } catch (InterruptedException e) {
             e.printStackTrace();
